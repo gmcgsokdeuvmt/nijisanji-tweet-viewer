9a4aa714c04eb59b0b7b4c5f4b52259c41cd2ab9
ran <mochimochi.morst@gmail.com>
2019-01-28 01:25:06 +0900

    add

--- util_io.js
+++ util_io.js
@@ -1,7 +1,7 @@
 async function getJSON(url) {
     var request = new XMLHttpRequest();
     request.onreadystatechange = function() {
-        if ((myJSON.readyState === 4) && (myJSON.status === 200)) {
+        if ((request.readyState === 4) && (request.status === 200)) {
             var text = request.responseText;
             var data = JSON.parse(text);
             return data;
