dd9f52f8311b467e8719151a9bc95b1808f148ad
ran <mochimochi.morst@gmail.com>
2019-01-28 01:45:09 +0900

    add

--- util_io.js
+++ util_io.js
@@ -1,7 +1,9 @@
 async function getJSON(url) {
     fetch(url).then((response) => {
         var text = response.text();
+        console.log(text);
         var data = JSON.parse(text);
+        console.log(data);
         return data;
       }).catch(() => {
         console.log("error caught!");
