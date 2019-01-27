c016aad5eeb37b9adb1a0c424c468c2078b7817c
ran <mochimochi.morst@gmail.com>
2019-01-28 01:38:13 +0900

    add

--- util_io.js
+++ util_io.js
@@ -1,13 +1,7 @@
 async function getJSON(url) {
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function() {
-        if ((request.readyState === 4) && (request.status === 200)) {
-            var text = request.responseText;
-            var data = JSON.parse(text);
-            console.log(data);
-            resolve(data);
-        }
-    }
-    request.open("GET",url, true);
-    request.send(null);
+    fetch(url).then((response) => {
+        console.log(response);
+      }).catch(() => {
+        console.log("error caught!");
+      });
 }
