33195ee8741208c4ead4b745dd59db0db716a7e3
ran <mochimochi.morst@gmail.com>
2019-01-28 01:18:21 +0900

    add

--- nijisanji.js
+++ nijisanji.js
@@ -1,12 +1,10 @@
-var myJSON = new XMLHttpRequest();
-myJSON.onreadystatechange = function() {
+var request = new XMLHttpRequest();
+request.onreadystatechange = function() {
     if ((myJSON.readyState === 4) && (myJSON.status === 200)) {
         var text = myJSON.responseText;
-        console.log(text);
         var data = JSON.parse(text);
         console.log(data);
-        alert(data.param);
     }
 }
-myJSON.open("GET","resource/nijisanji.json", true);
-myJSON.send(null);
+request.open("GET","resource/nijisanji.json", true);
+request.send(null);
